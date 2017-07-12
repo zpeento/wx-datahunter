1705b0018af7d580cfc65a68a330921b9494b36c
iceiceice <509887158@qq.com>
2017-07-13 00:28:39 +0800

    change:  增加swiper效果来展示案例

--- pages/case/case.js
+++ pages/case/case.js
@@ -3,7 +3,17 @@
 var app = getApp()
 Page({
   data: {
-    tab_index: '0'
+    tab_index: '0',
+    swiper: {
+      imgUrls: [   'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      ],
+      indicatorDots: true,
+      autoplay: true,
+      interval: 2500,
+      duration: 1000
+    }
   },
   onLoad:function(options){
     this.setData({

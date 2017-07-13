10ff74a6b82e936fd8fe968e4dd70c10fcfcbab1
iceiceice <509887158@qq.com>
2017-07-14 00:41:06 +0800

    change:  在报价旁边添加call功能

--- pages/case/case.js
+++ pages/case/case.js
@@ -42,15 +42,9 @@ Page({
       tab_index:"2"
     })
   },
-  // onLoad: function () {
-  //   console.log('onLoad')
-  //   var that = this
-  //   //调用应用实例的方法获取全局数据
-  //   app.getUserInfo(function(userInfo){
-  //     //更新数据
-  //     that.setData({
-  //       userInfo:userInfo
-  //     })
-  //   })
-  // }
+  call: function() {
+    wx.makePhoneCall({
+      phoneNumber: '15811872632' //仅为示例，并非真实的电话号码
+    })
+  }
 })
